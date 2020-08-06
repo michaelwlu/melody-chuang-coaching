6390b49e9686f6d231b675f2e1f54418e29b4f2d
michaelwlu <michael.lu@stern.nyu.edu>
2020-08-06 09:42:42 -0400

    Test Version

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,5 +1,3 @@
-const tailwindcss = require('tailwindcss');
-
 module.exports = {
-  plugins: [tailwindcss('./tailwind.config.js'), require('autoprefixer')],
+  plugins: [require('tailwindcss'), require('autoprefixer')],
 };
