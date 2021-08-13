90832533f5bf856e31404627919d753dc0378562
Michael W Lu <michaelwenlu@gmail.com>
2021-08-12 23:11:11 -0400

    Clear contact form after successful submission

--- website/src/components/forms/ContactBox.js
+++ website/src/components/forms/ContactBox.js
@@ -1,116 +1,117 @@
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import ContactInfo from './ContactInfo';
-import { sendContact } from './FormSubmission';
-import SubmitButton from './SubmitButton';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import ContactInfo from "./ContactInfo";
+import { sendContact } from "./FormSubmission";
+import SubmitButton from "./SubmitButton";
 
 const emptyContactInfo = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  message: '',
+	firstName: "",
+	lastName: "",
+	email: "",
+	message: "",
 };
 
 const testContactInfo = {
-  firstName: 'TestFirst',
-  lastName: 'TestLast',
-  email: 'TestEmail@test.com',
-  message: String(Date.now()),
+	firstName: "TestFirst",
+	lastName: "TestLast",
+	email: "TestEmail@test.com",
+	message: String(Date.now()),
 };
 
 const ContactBox = () => {
-  const [contactInfo, setContactInfo] = useState(emptyContactInfo);
+	const [contactInfo, setContactInfo] = useState(emptyContactInfo);
 
-  const [contactError, setContactError] = useState([]);
-  const [contactSuccess, setContactSuccess] = useState(false);
+	const [contactError, setContactError] = useState([]);
+	const [contactSuccess, setContactSuccess] = useState(false);
 
-  const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const requireAll = true;
+	const requireAll = true;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setIsLoading(true);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setIsLoading(true);
 
-    const newContact = {
-      id: uuidv4(),
-      ...contactInfo,
-    };
+		const newContact = {
+			id: uuidv4(),
+			...contactInfo,
+		};
 
-    sendContact(newContact)
-      .then((res) => {
-        console.log(res);
-        switch (res.status) {
-          case 201:
-            setContactSuccess(true);
-            break;
-          case 400:
-            setContactError(res.data.error);
-            break;
-          case 500:
-            setContactError([res.statusText]);
-            break;
-          default:
-            setContactError(['Unknown error, please try again later']);
-        }
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setContactError(['Unknown error, please try again later']);
-        setIsLoading(false);
-      });
-  };
+		sendContact(newContact)
+			.then((res) => {
+				console.log(res);
+				switch (res.status) {
+					case 201:
+						setContactSuccess(true);
+						setContactInfo(emptyContactInfo);
+						break;
+					case 400:
+						setContactError(res.data.error);
+						break;
+					case 500:
+						setContactError([res.statusText]);
+						break;
+					default:
+						setContactError(["Unknown error, please try again later"]);
+				}
+				setIsLoading(false);
+			})
+			.catch((err) => {
+				setContactError(["Unknown error, please try again later"]);
+				setIsLoading(false);
+			});
+	};
 
-  const updateField = (e) => {
-    setContactInfo({
-      ...contactInfo,
-      [e.target.name]: e.target.value,
-    });
-  };
+	const updateField = (e) => {
+		setContactInfo({
+			...contactInfo,
+			[e.target.name]: e.target.value,
+		});
+	};
 
-  return (
-    <div className="flex-shrink w-full max-w-xl">
-      <h1 className="text-xl font-semibold leading-tight lg:text-2xl font-themeSerif">
-        Contact Me
-      </h1>
-      <form onSubmit={handleSubmit} className="relative mt-4 font-sans text-sm">
-        <ContactInfo
-          info={{
-            firstName: contactInfo.firstName,
-            lastName: contactInfo.lastName,
-            email: contactInfo.email,
-          }}
-          updateField={updateField}
-          showIcons={true}
-          requireAll={requireAll}
-          isSmall={true}
-        />
-        <div className="block mt-2 mb-4 md:mb-6">
-          <label htmlFor="message">Message</label>
-          <textarea
-            id="message"
-            name="message"
-            required={requireAll}
-            maxLength={10000}
-            value={contactInfo.message}
-            onChange={updateField}
-            className="block w-full mt-2 text-sm bg-gray-100 border-opacity-50 border-themeblue-300 form-textarea"
-            rows="5"
-          />
-        </div>
-        <SubmitButton isLoading={isLoading} />
-        <div className="mt-3 text-xs text-red-700">
-          {contactError &&
-            contactError.map((msg, idx) => <p key={idx}>{msg}</p>)}
-        </div>
-      </form>
-      {contactSuccess ? (
-        <div className="mt-6 font-sans text-sm">
-          Thanks for your message! I'll be reaching out to you soon.
-        </div>
-      ) : null}
-    </div>
-  );
+	return (
+		<div className="flex-shrink w-full max-w-xl">
+			<h1 className="text-xl font-semibold leading-tight lg:text-2xl font-themeSerif">
+				Contact Me
+			</h1>
+			<form onSubmit={handleSubmit} className="relative mt-4 font-sans text-sm">
+				<ContactInfo
+					info={{
+						firstName: contactInfo.firstName,
+						lastName: contactInfo.lastName,
+						email: contactInfo.email,
+					}}
+					updateField={updateField}
+					showIcons={true}
+					requireAll={requireAll}
+					isSmall={true}
+				/>
+				<div className="block mt-2 mb-4 md:mb-6">
+					<label htmlFor="message">Message</label>
+					<textarea
+						id="message"
+						name="message"
+						required={requireAll}
+						maxLength={10000}
+						value={contactInfo.message}
+						onChange={updateField}
+						className="block w-full mt-2 text-sm bg-gray-100 border-opacity-50 border-themeblue-300 form-textarea"
+						rows="5"
+					/>
+				</div>
+				<SubmitButton isLoading={isLoading} />
+				<div className="mt-3 text-xs text-red-700">
+					{contactError &&
+						contactError.map((msg, idx) => <p key={idx}>{msg}</p>)}
+				</div>
+			</form>
+			{contactSuccess ? (
+				<div className="mt-6 font-sans text-sm">
+					Thanks for your message! I'll be reaching out to you soon.
+				</div>
+			) : null}
+		</div>
+	);
 };
 
 export default ContactBox;
