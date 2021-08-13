d22ec35dfec07d9c679db1742ed23b421cb1d940
Michael W Lu <michaelwenlu@gmail.com>
2021-08-12 20:34:40 -0400

    Fix email config

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,85 +1,85 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const {
-  mainContactFormat,
-  clientContactFormat,
-  mainApplicationFormat,
-  clientApplicationFormat,
-} = require('./emailFormatter');
+	mainContactFormat,
+	clientContactFormat,
+	mainApplicationFormat,
+	clientApplicationFormat,
+} = require("./emailFormatter");
 
 async function mailer(type, body) {
-  try {
-    let gmailTransportConfig = {
-      service: 'gmail',
-      auth: {
-        type: 'OAuth2',
-        user: process.env.GMAIL_USER,
-        clientId: process.env.GMAIL_CLIENT_ID,
-        clientSecret: process.env.GMAIL_CLIENT_SECRET,
-        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    };
+	try {
+		let gmailTransportConfig = {
+			service: "gmail",
+			auth: {
+				type: "OAuth2",
+				user: process.env.GMAIL_CLIENT_USER,
+				clientId: process.env.GMAIL_CLIENT_ID,
+				clientSecret: process.env.GMAIL_CLIENT_SECRET,
+				refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+			},
+			tls: {
+				rejectUnauthorized: false,
+			},
+		};
 
-    let transporter = nodemailer.createTransport(gmailTransportConfig);
+		let transporter = nodemailer.createTransport(gmailTransportConfig);
 
-    let mainHeaders = {
-      from: process.env.GMAIL_USER,
-      to: process.env.GMAIL_USER,
-    };
+		let mainHeaders = {
+			from: process.env.GMAIL_CLIENT_USER,
+			to: process.env.BUSINESS_EMAIL,
+		};
 
-    let clientHeaders = {
-      from: `Melody <${process.env.GMAIL_USER}>`,
-      to: body.email,
-    };
+		let clientHeaders = {
+			from: `Melody <${process.env.BUSINESS_EMAIL}>`,
+			to: body.email,
+		};
 
-    let mainConfig =
-      type === 'contact'
-        ? {
-            subject: 'New Contact Request',
-            html: mainContactFormat(body),
-          }
-        : type === 'application'
-        ? {
-            subject: 'New Application',
-            html: mainApplicationFormat(body),
-          }
-        : {
-            subject: 'Unknown Error',
-            text: 'Unknown Mailer Error',
-          };
+		let mainConfig =
+			type === "contact"
+				? {
+						subject: "New Contact Request",
+						html: mainContactFormat(body),
+				  }
+				: type === "application"
+				? {
+						subject: "New Application",
+						html: mainApplicationFormat(body),
+				  }
+				: {
+						subject: "Unknown Error",
+						text: "Unknown Mailer Error",
+				  };
 
-    let clientConfig =
-      type === 'contact'
-        ? {
-            subject: 'Thank you for your message!',
-            html: clientContactFormat(body),
-          }
-        : type === 'application'
-        ? {
-            subject: 'Your application has been received!',
-            html: clientApplicationFormat(body),
-          }
-        : {
-            subject: 'Unknown Error',
-            text: 'Unknown Mailer Error',
-          };
+		let clientConfig =
+			type === "contact"
+				? {
+						subject: "Thank you for your message!",
+						html: clientContactFormat(body),
+				  }
+				: type === "application"
+				? {
+						subject: "Your application has been received!",
+						html: clientApplicationFormat(body),
+				  }
+				: {
+						subject: "Unknown Error",
+						text: "Unknown Mailer Error",
+				  };
 
-    let mainInfo = await transporter.sendMail({
-      ...mainHeaders,
-      ...mainConfig,
-    });
-    console.log(`Message sent: ${mainInfo.messageId}`);
+		let mainInfo = await transporter.sendMail({
+			...mainHeaders,
+			...mainConfig,
+		});
+		console.log(`Message sent: ${mainInfo.messageId}`);
 
-    let clientInfo = await transporter.sendMail({
-      ...clientHeaders,
-      ...clientConfig,
-    });
-    console.log(`Message sent: ${clientInfo.messageId}`);
-  } catch (error) {
-    console.error(`Mailer Error: ${error}`);
-  }
+		let clientInfo = await transporter.sendMail({
+			...clientHeaders,
+			...clientConfig,
+		});
+		console.log(`Message sent: ${clientInfo.messageId}`);
+	} catch (error) {
+		console.error(`Mailer Error: ${error}`);
+	}
 }
 
 module.exports = mailer;
